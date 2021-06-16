be4808f4b7c84d6617682ef20b148c2f56441e41
vj <venkatesh.jatla@gmail.com>
2021-06-16 08:14:14 -0600

    My first commit

---
+++ pytkit/pytkit/__init__.py
@@ -0,0 +1,6 @@
+from aqua.fd_ops import get_file_paths_with_kws
+from aqua.video_tools import Vid
+from pytkit.fd import 
+
+# Load moduels from fd_ops.py
+__all__ = ["get_file_paths_with_kws", "Vid"]
---
+++ pytkit/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+from setuptools import setup
+setup(
+    name="pytkit",
+    version="0.0",
+    description="""Python modules that can be shared across projects.""",
+    url="",
+    author="Vj",
+    author_email="venkatesh.jatla@gmail.com",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
